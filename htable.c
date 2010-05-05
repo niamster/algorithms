49858a0872a4383517c933987caffc94ba201ca7
Dmytro Milinevskyy <milinevskyy@gmail.com>
2010-05-05 23:20:30 +0300

    [htable] print htable summary; return set of matched values for the key

--- htable.c
+++ htable.c
@@ -91,39 +91,66 @@ print_data(struct hash_data *data,
 }
 
 void
-print_table(struct hash_node **hash_table,
+print_htable_entries(struct hash_node *node)
+{
+    while (node) {
+        printf("    '%s' => '%s'\n", node->data->key, node->data->value);
+        node = node->next;
+    }
+}
+
+void
+print_htable(struct hash_node **hash_htable,
             int hash_size)
 {
-    printf("table of %d entries\n", hash_size);
+    printf("htable of %d entries\n", hash_size);
 
     int i;
     for (i=0;i<hash_size;++i) {
-        struct hash_node *node = hash_table[i];
         printf("Level %d\n", i);
 
+        print_htable_entries(hash_htable[i]);
+    }
+}
+
+
+void
+print_htable_summary(struct hash_node **hash_htable,
+                    int hash_size,
+                    int count)
+{
+    printf("htable of %d entries\n", hash_size);
+
+    int i;
+    for (i=0;i<hash_size;++i) {
+        struct hash_node *node = hash_htable[i];
+        int level_count = 0;
+
         while (node) {
-            printf("    '%s' => '%s'\n", node->data->key, node->data->value);
+            ++level_count;
             node = node->next;
         }
+
+        printf("Level %5d: %5d entries(%.2f%%)\n", i, level_count, ((float)level_count/(float)count)*100);
     }
 }
 
 int
 contruct_htable(struct hash_data *data,
                 int count,
-                struct hash_node ***hash_table,
+                struct hash_node ***hash_htable,
                 int hash_size,
                 hash_function_t hash_function)
 {
     int i;
 
-    if (!(*hash_table = malloc(hash_size*sizeof(struct hash_node *)))) {
+    if (!(*hash_htable = malloc(hash_size*sizeof(struct hash_node *)))) {
         fprintf(stderr, "Error error allocating %d bytes: %s", hash_size*sizeof(struct hash_node *), strerror(errno));
         return -1;
     }
 
     for (i=0;i<hash_size;++i)
-        (*hash_table)[i] = NULL;
+        (*hash_htable)[i] = NULL;
 
     for (i=0;i<count;++i) {
         struct hash_node *node = NULL;
@@ -139,35 +166,39 @@ contruct_htable(struct hash_data *data,
         }
 
         node->data = &data[i];
-        node->next = (*hash_table)[hash];
-        (*hash_table)[hash] = node;
+        node->next = (*hash_htable)[hash];
+        (*hash_htable)[hash] = node;
     }
 }
 
 void
-destroy_htable(struct hash_node **hash_table,
+destroy_htable_entries(struct hash_node *node)
+{
+    while (node) {
+        struct hash_node *n = node;
+        node = node->next;
+        free(n);
+    }
+}
+
+void
+destroy_htable(struct hash_node **hash_htable,
                     int hash_size)
 {
     int i;
-    for (i=0;i<hash_size;++i) {
-        struct hash_node *node = hash_table[i];
+    for (i=0;i<hash_size;++i)
+        destroy_htable_entries(hash_htable[i]);
 
-        while (node) {
-            struct hash_node *n = node;
-            node = node->next;
-            free(n);
-        }
-    }
-
-    free(hash_table);
+    free(hash_htable);
 }
 
-unsigned char *
-search_htable(struct hash_node **hash_table,
+struct hash_node *
+search_htable(struct hash_node **hash_htable,
               int hash_size,
               hash_function_t hash_function,
               const unsigned char *key)
 {
+    struct hash_node *result = NULL;
     struct hash_node *node;
     int hash = hash_function(key, hash_size-1);
     if (hash >= hash_size) { /* sanity checks */
@@ -175,19 +206,32 @@ search_htable(struct hash_node **hash_table,
         return NULL;
     }
 
-    node = hash_table[hash];
+    node = hash_htable[hash];
 
     while (node) {
+        struct hash_node *needle;
+
         if (!key) {
-            if (!node->data->key)
-                return node->data->value;
-        } else if (!strcmp(key, node->data->key)) {
-            return node->data->value;
+            if (node->data->key)
+                goto next;
+        } else if (strcmp(key, node->data->key)) {
+            goto next;
         }
+
+        if (!(needle = malloc(hash_size*sizeof(struct hash_node)))) {
+            fprintf(stderr, "Error error allocating %d bytes: %s", sizeof(struct hash_node), strerror(errno));
+            goto out;
+        }
+        needle->data = node->data;
+        needle->next = result;
+        result = needle;
+
+      next:
         node = node->next;
     }
 
-    return NULL;
+  out:
+    return result;
 }
 
 int
@@ -196,6 +240,9 @@ simple_hash(const char *data,
 {
     unsigned int hash = 0;
 	int c;
+
+    if (!data || limit == 0)
+        return 0;
 
 	while (c = *data++)
 	    hash += c;
@@ -212,7 +259,7 @@ int
 usage(const char *prog)
 {
     fprintf(stderr, "Usage:\n");
-    fprintf(stderr, "%s --hash-function|-f simple --hash-size|-s size --key|-k key [--input-data|-i <path>] [--count|-c <num>] [--dump-data] [--dump-table]\n", prog);
+    fprintf(stderr, "%s --hash-function|-f simple --hash-size|-s size --key|-k key [--input-data|-i <path>] [--count|-c <num>] [--dump-data] [--dump-htable] [--dump-htable-summary]\n", prog);
     return 1;
 }
 
@@ -221,15 +268,15 @@ int main(int argc, char **argv)
     struct timeval tb, ta;
     unsigned long secs, msecs, usecs;
 
-    int dump_data = 0, dump_table = 0;
+    int dump_data = 0, dump_htable = 0, dump_htable_summary = 0;
     hash_function_t hash_function = NULL;
     int hash_size = 0;
-    struct hash_node **hash_table = NULL;
+    struct hash_node **hash_htable = NULL;
     int count = 0;
     const char *input_data = "/dev/random";
     struct hash_data *data;
     const unsigned char *key = NULL;
-    unsigned  char *value;
+    struct hash_node *values;
 
     int opt;
     const struct option options[] = {
@@ -238,7 +285,8 @@ int main(int argc, char **argv)
         {"key",           required_argument, NULL, 'k'},
         {"input-data",    required_argument, NULL, 'i'},
         {"count",         required_argument, NULL, 'c'},
-        {"dump-table",    no_argument,       &dump_table, 1},
+        {"dump-htable",    no_argument,       &dump_htable, 1},
+        {"dump-htable-summary",  no_argument, &dump_htable_summary, 1},
         {"dump-data",     no_argument,       &dump_data, 1},
         {0,               0,                 0,    0}
     };
@@ -278,18 +326,23 @@ int main(int argc, char **argv)
     if (dump_data)
         print_data(data, count);
 
-    contruct_htable(data, count, &hash_table, hash_size, hash_function);
-    if (dump_table)
-        print_table(hash_table, hash_size);
+    contruct_htable(data, count, &hash_htable, hash_size, hash_function);
+    if (dump_htable)
+        print_htable(hash_htable, hash_size);
+    if (dump_htable_summary)
+        print_htable_summary(hash_htable, hash_size, count);
 
     gettimeofday(&tb, NULL);
-    value = search_htable(hash_table, hash_size, hash_function, key);
+    values = search_htable(hash_htable, hash_size, hash_function, key);
     gettimeofday(&ta, NULL);
 
-    if (value)
-        printf("Value '%s' for key '%s'\n", value, key);
-    else
+    if (values) {
+        printf("Entries for key '%s'\n", key);
+        print_htable_entries(values);
+        destroy_htable_entries(values);
+    } else {
         printf("Value was not found for key '%s'\n", key);
+    }
 
     usecs = ta.tv_sec*1000000 + ta.tv_usec - tb.tv_sec*1000000 - tb.tv_usec;
     secs = usecs/1000000;
@@ -300,7 +353,7 @@ int main(int argc, char **argv)
 
   out:
     free(data);
-    destroy_htable(hash_table, hash_size);
+    destroy_htable(hash_htable, hash_size);
 
     return 0;
 }
