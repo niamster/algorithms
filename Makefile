c18c06c2cc1f74a350c20c5b9d4c7c85ca6b090a
Dmytro Milinevskyy <milinevskyy@gmail.com>
2012-04-09 20:56:51 +0300

    [moving average] initial implementation

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 CC		:= gcc
 CFLAGS	:= -O3
-ALGOS	:= qsort binary-search htable-list htable-tree binary-tree binary-tree-avl binary-tree-rb wq kalman
+ALGOS	:= qsort binary-search htable-list htable-tree binary-tree binary-tree-avl binary-tree-rb wq kalman moving-average
 LDFLAGS := -lpthread
 
 RND_CNT := 1000000
@@ -60,6 +60,9 @@ wq: workqueue.c notification.c $(HELPERS)
 
 kalman: kalman.c $(HELPERS)
 	$(Q)$(CC) -DKALMAN_MAIN $^ $(CFLAGS) -o $@ $(LDFLAGS)
+
+moving-average: moving_average.c $(HELPERS)
+	$(Q)$(CC) -DMOVING_AVERAGE_MAIN $^ $(CFLAGS) -o $@ $(LDFLAGS)
 
 .PHONY: rnd.u32 rnd.32b
 rnd-u32:
@@ -147,6 +150,24 @@ test-kalman: kalman
 						 '$@.$(TST_RND_CNT).3.out' using 1:3 smooth csplines with linespoints title 'Kalman-Q15-R15', \
 						 '$@.$(TST_RND_CNT).4.out' using 1:3 smooth csplines with linespoints title 'Kalman-Q20-R15', \
 						 '$@.$(TST_RND_CNT).5.out' using 1:3 smooth csplines with linespoints title 'Kalman-Q20-R50'"
+
+test-moving-average: moving-average
+	$(Q)if test "$(TST_REGEN_RND)" = "yes" -o ! -f $@.$(TST_RND_CNT).rnd; then dd if=/dev/urandom of=$@.$(TST_RND_CNT).rnd bs=$(TST_RND_CNT) count=4 status=noxfer >/dev/null 2>&1; fi
+
+	$(Q)echo "moving-average"
+	$(Q)./moving-average -P3 -i $@.$(TST_RND_CNT).rnd -o $@.$(TST_RND_CNT).0.out $(RFLAGS)
+	$(Q)./moving-average -P5 -i $@.$(TST_RND_CNT).rnd -o $@.$(TST_RND_CNT).1.out $(RFLAGS)
+	$(Q)./moving-average -P20 -i $@.$(TST_RND_CNT).rnd -o $@.$(TST_RND_CNT).2.out $(RFLAGS)
+	$(Q)gnuplot -e "set terminal png size $(TST_RND_CNT)*50,1400; \
+					set key left box outside; \
+					set title 'Moving-Average filter'; \
+					set output '$@.$(TST_RND_CNT).png'; \
+					set xtics 10; \
+					set mxtics 10; \
+					plot '$@.$(TST_RND_CNT).0.out' using 1:2 smooth csplines with linespoints lc rgb 'red' lw 2 title 'Unfiltered', \
+						 '$@.$(TST_RND_CNT).0.out' using 1:3 smooth csplines with linespoints title 'Moving-Average-3', \
+						 '$@.$(TST_RND_CNT).1.out' using 1:3 smooth csplines with linespoints title 'Moving-Average-5', \
+						 '$@.$(TST_RND_CNT).2.out' using 1:3 smooth csplines with linespoints title 'Moving-Average-20'"
 
 
 clean:
---
+++ moving_average.c
@@ -0,0 +1,189 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/time.h>
+#include <errno.h>
+#include <getopt.h>
+
+#include "helpers.h"
+
+struct moving_average_filter_state {
+    int idx;
+    int P;
+    double *b;
+    double *samples;
+};
+
+int moving_average_filter_init(struct moving_average_filter_state *state)
+{
+    state->idx = 0;
+
+    state->b = malloc(state->P * sizeof(double));
+    if (!state->b) {
+        fprintf(stderr, "Error error allocating %d bytes: %s", state->P * sizeof(double), strerror(errno));
+        return -1;
+    }
+
+    {
+        int i;
+        double b = 1./(double)state->P;
+        for (i=0;i<state->P;++i)
+            state->b[i] = b;
+    }
+
+    state->samples = calloc(state->P, sizeof(double));
+    if (!state->samples) {
+        fprintf(stderr, "Error error allocating %d bytes: %s", state->P * sizeof(double), strerror(errno));
+        free(state->b);
+        return -1;
+    }
+
+    return 0;
+}
+
+void moving_average_filter_destroy(struct moving_average_filter_state *state)
+{
+    if (state->b)
+        free(state->b);
+    if (state->samples)
+        free(state->samples);
+}
+
+double moving_average_filter(struct moving_average_filter_state *state, double in)
+{
+    double acc = 0;
+    int i;
+
+    state->samples[state->idx] = in;
+
+    for (i=0;i<state->P;++i)
+        acc += state->samples[i]*state->b[i];
+
+    ++state->idx;
+    state->idx %= state->P;
+
+    return acc;
+}
+
+#ifdef MOVING_AVERAGE_MAIN
+int
+usage(const char *prog)
+{
+    fprintf(stderr, "Usage:\n");
+    fprintf(stderr, "%s [--input-data|-i <path>] [--count|-c <num>] [--output-data|-o <path>]\n", prog);
+    return 1;
+}
+
+int main(int argc, char **argv)
+{
+    int ret = 1;
+
+    const char *input_data = "/dev/random", *output_data = NULL;
+    int count = 0;
+
+    struct moving_average_filter_state ma_state;
+    unsigned int *array_in, *array_out;
+
+    int opt;
+    const struct option options[] = {
+        {"order",             required_argument, NULL, 'P'},
+        {"count",             required_argument, NULL, 'c'},
+        {"input-data",        required_argument, NULL, 'i'},
+        {"output-data",       required_argument, NULL, 'o'},
+        {0,                   0,                 0,    0}
+    };
+
+    memset(&ma_state, 0x0, sizeof(ma_state));
+
+    while ((opt = getopt_long(argc, argv, "P:c:i:o:", options, NULL)) != -1) {
+        switch (opt) {
+            case 'P':
+                ma_state.P = atof(optarg);
+                break;
+            case 'c':
+                count = atoi(optarg);
+                break;
+            case 'i':
+                input_data = optarg;
+                break;
+            case 'o':
+                output_data = optarg;
+                break;
+            case 0:
+                break;
+            default:
+                return usage(argv[0]);
+        }
+    }
+
+    if (generate_array(&array_in, &count, input_data) == -1)
+        return 1;
+
+    printf("Elements: %d\n", count);
+    {
+        int i;
+        for (i=0;i<count;++i)
+            array_in[i] %= 100;
+    }
+
+    if (moving_average_filter_init(&ma_state))
+        goto out;
+
+    printf("Moving Average parameters: P:%d\n",
+            ma_state.P);
+    {
+        int i;
+
+        printf("b:\n");
+        for (i=0;i<ma_state.P;++i)
+            printf("%f ", ma_state.b[i]);
+        printf("\n");
+    }
+
+    array_out = malloc(sizeof(unsigned int)*count);
+    if (!array_out) {
+        fprintf(stderr, "Unable to allocate %d bytes", count);
+        goto out;
+    }
+    memset(array_out, 0x0, sizeof(unsigned int)*count);
+
+    {
+        int i;
+
+        for (i=0;i<count;++i)
+            array_out[i] = moving_average_filter(&ma_state, array_in[i]);
+    }
+
+    if (output_data) {
+        int i;
+        int d = open(output_data, O_WRONLY|O_CREAT, 0666);
+        if (d == -1) {
+            fprintf(stderr, "Error opening %s: %s", output_data, strerror(errno));
+            goto out;
+        }
+        ftruncate(d, 0);
+        for (i=0;i<count;++i) {
+            char b[1024] = {0x0, };
+            int n;
+            n = snprintf(b, sizeof(b), "%d %d %d\n", i, array_in[i], array_out[i]);
+            if (write(d, b, n) == -1) {
+                fprintf(stderr, "Error error writing to %s: %s", output_data, strerror(errno));
+                break;
+            }
+        }
+        close(d);
+    }
+
+    ret = 0;
+  out:
+    moving_average_filter_destroy(&ma_state);
+    if (array_in)
+        free(array_in);
+    if (array_out)
+        free(array_out);
+
+    return ret;
+}
+#endif
